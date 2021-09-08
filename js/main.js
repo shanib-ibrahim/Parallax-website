1b703548e5e0867231afc47affc56d551ceb2f2f
shanib-ibrahim <shanibibrahimcm@gmail.com>
2021-09-08 13:07:27 +0530

    initial commit of parallax website

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+
+timeline
+ .to(".container__rock",3,{y:-300})
+ .to(".container__girl",3,{y:-200},"-=3")
+ .fromTo(".container__background",{y:-50},{y:0,duration:3},"-=3")
+ .to(".content",3,{top:"0%"},"-=3")
+ .fromTo(".content__images",{opacity:0},{opacity:1,duration:3})
+ .fromTo(".content__text",{opacity:0},{opacity:1,duration:3}); 
+
+ let scene =new ScrollMagic.Scene({
+     triggerElement:"section",
+     duration:"150%",
+     triggerHook:0,
+ })
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller);
